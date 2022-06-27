8d48a64ca2fe5387c6722738d348d781500ab68e
Sergei Minaev <brainsburn@yandex.ru>
2022-06-27 12:05:45 +0300

    Create pidfile for future use

--- src/server.rs
+++ src/server.rs
@@ -1,10 +1,13 @@
 use std::net::{Ipv4Addr, IpAddr, SocketAddr, TcpListener};
 use std::str::FromStr;
 use async_io::{Async};
+use qpidfile::Pidfile;
 use crate::conf::CONF;
 
 
 pub async fn run() {
+    let _pidfile = Pidfile::new("miarh.pid").unwrap();
+
     let conf = CONF.read().await;
     let tls_addr: SocketAddr = SocketAddr::new(IpAddr::V4(Ipv4Addr::from_str(&conf.ip).unwrap()), conf.tls_port);
     let _tls_listener = Async::<TcpListener>::bind(tls_addr).unwrap();
