010cc61dca3bc7991565cedac7a675ffa0af2b23
Sergei Minaev <brainsburn@yandex.ru>
2022-06-27 12:26:08 +0300

    Limit line len to 100 chars

--- src/server.rs
+++ src/server.rs
@@ -10,11 +10,15 @@ pub async fn run() {
     let _pidfile = Pidfile::new("miarh.pid").unwrap();
 
     let conf = CONF.read().await;
-    let tls_addr: SocketAddr = SocketAddr::new(IpAddr::V4(Ipv4Addr::from_str(&conf.ip).unwrap()), conf.tls_port);
-    let tcp_addr: SocketAddr = SocketAddr::new(IpAddr::V4(Ipv4Addr::from_str(&conf.ip).unwrap()), conf.tcp_port);
+    let tls_addr: SocketAddr = SocketAddr::new(
+        IpAddr::V4(Ipv4Addr::from_str(&conf.ip).unwrap()), conf.tls_port);
+    let tcp_addr: SocketAddr = SocketAddr::new(
+        IpAddr::V4(Ipv4Addr::from_str(&conf.ip).unwrap()), conf.tcp_port);
     let _tls_listener = Async::<TcpListener>::bind(tls_addr).unwrap();
     let _tcp_listener = Async::<TcpListener>::bind(tcp_addr).unwrap();
 
-    let tls_identity = Identity::from_pkcs12(include_bytes!("../identity.pfx"), "").unwrap();
-    let _tls_acceptor = TlsAcceptor::from(native_tls::TlsAcceptor::new(tls_identity).unwrap());
+    let tls_identity = Identity::from_pkcs12(
+        include_bytes!("../identity.pfx"), "").unwrap();
+    let _tls_acceptor = TlsAcceptor::from(
+        native_tls::TlsAcceptor::new(tls_identity).unwrap());
 }
