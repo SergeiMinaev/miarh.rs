80573c303c3f9cd8c021932d1ceca444316f4ed8
Sergei Minaev <brainsburn@yandex.ru>
2026-06-02 03:12:17 +0300

    Headers: allow x-diag-token

--- src/headers.rs
+++ src/headers.rs
@@ -373,7 +373,7 @@ fn parse_sec_ws_key(s: &str, r: &mut HashMap<String, String>) {
 
 fn parse_whitelisted_header(lower: &str, original: &str, filtered: &mut HashMap<String, String>) {
     // Whitelist of headers we want to preserve (case-insensitive).
-    const WHITELIST: [&str; 2] = ["x-tracker-upload-token", "x-device-id"];
+    const WHITELIST: [&str; 3] = ["x-tracker-upload-token", "x-device-id", "x-diag-token"];
     for key in WHITELIST {
         if lower.starts_with(&(key.to_string() + ": ")) {
             if let Some((_, value)) = original.split_once(':') {
