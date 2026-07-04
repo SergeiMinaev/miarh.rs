9a5719b3aca27e3463129cc9132693246e79d3c6
Sergei Minaev <brainsburn@yandex.ru>
2026-07-04 21:56:07 +0300

    Referer добавлен в белый список заголовков

--- src/headers.rs
+++ src/headers.rs
@@ -378,13 +378,14 @@ fn parse_sec_ws_key(s: &str, r: &mut HashMap<String, String>) {
 
 fn parse_whitelisted_header(lower: &str, original: &str, filtered: &mut HashMap<String, String>) {
     // Whitelist of headers we want to preserve (case-insensitive).
-    const WHITELIST: [&str; 6] = [
+    const WHITELIST: [&str; 7] = [
         "x-tracker-upload-token",
         "x-device-id",
         "x-diag-token",
         "x-release-token",
         "x-app-version",
         "x-app-platform",
+        "referer",
     ];
     for key in WHITELIST {
         if lower.starts_with(&(key.to_string() + ": ")) {
